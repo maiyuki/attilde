0de00385841ae62c7a695e1c40d34ab67d731a7b
maiyuki <hello@maiyuki.com>
2022-05-29 15:09:39 +0200

    Add cookies.html and update typos, text and other

--- terraform/attilde.tf
+++ terraform/attilde.tf
@@ -3,6 +3,7 @@ module "attilde_static_website" {
   region = "eu-north-1"
   name_prefix = "attilde"
   website_domain_name = "attilde.com"
+  comment_for_cloudfront_website = "Managed by terraform"
   tags = {
       Description = "Managed by terraform"
   }
